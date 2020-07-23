7f9f8a5a4907d20aac5e85a59f13cf531603d1ff
dan <2207161187@qq.com>
2020-07-23 22:23:37 +0800

    [add] 添加自定义数据池连接_v2

--- src/main/java/com/itdan/spring_demo/db_pool/utils/DBConnectionPool.java
+++ src/main/java/com/itdan/spring_demo/db_pool/utils/DBConnectionPool.java
@@ -4,6 +4,7 @@ import com.itdan.spring_demo.db_pool.bean.PoolConfig;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.SQLException;
 import java.util.List;
 import java.util.Vector;
 
@@ -31,6 +32,9 @@ public class DBConnectionPool {
     private List<Connection> activePool=new Vector<>();
     //连接参数
     private PoolConfig poolConfig;
+
+    //连接池中现存在的连接总数(当前创建的连接数)
+    private int count;
 
     public DBConnectionPool(PoolConfig poolConfig) {
         //获取配置文件信息
@@ -61,7 +65,7 @@ public class DBConnectionPool {
     /**
      * 创建线程连接
      */
-    private Connection newConnection() {
+    private synchronized Connection newConnection() {
         //初始化线程,并将初始化后的线程添加到连接池中
         Connection  connection=null;
         try
@@ -75,6 +79,7 @@ public class DBConnectionPool {
                       poolConfig.getUrl(),
                       poolConfig.getUserName(),
                       poolConfig.getPassWord());
+            count++;
         }
         catch (Exception e)
         {
@@ -90,14 +95,77 @@ public class DBConnectionPool {
      * @return
      */
     public Connection getConnection(){
+        Connection connection=null;
+        //判断当前已创建连接数是否小于最大可创建连接数
+          if(count<poolConfig.getMaxActiveConn()){
+               //当前已创建连接数小于最大值时
+               //先判断空闲线程是否有资源
+              if(freePool.size()>0){
+                     //当空闲线程还存在连接对象时，直接从空闲线程池中获取
+                  connection = freePool.remove(0);
+              }else {
+                   //当空闲线程池中不存在连接对象并且可创建的连接数没有超过最大连接数,可直接创建连接对象
+                 connection= newConnection();
+              }
+              //判断连接对象是否可用
+              boolean result = isAvailable(connection);
+              if(result){
+              //将连接对象存储到活动线程中
+              activePool.add(connection);
+              }else {
+                  //重试机制，重新获取对象（使用递归算法）
+                  count--;
+                  connection = getConnection();
+              }
+          }else {
+             //大于最大连接数，配值waitTime=1000，表示大于连接数时，进行一秒钟的等待
+              try {
+                  wait(poolConfig.getWaitTime());
+                  connection = getConnection();
+              } catch (InterruptedException e) {
+                  e.printStackTrace();
+              }
+          }
+        return connection;
+    }
 
-        return null;
+    /**
+     * 判断连接对象是否可用
+     */
+    private boolean isAvailable(Connection connection) {
+        try {
+            if(connection==null || connection.isClosed()){
+                return false;
+            }
+        }catch (Exception e){
+            System.out.println(e.getMessage());
+        }
+        return true;
     }
 
     /**
      * 释放资源，回收连接对象
      */
-    public void releaseConnection(){
+    public synchronized void releaseConnection(Connection connection){
 
+        //判断连接对象是否可用
+        if(isAvailable(connection)){
+            //判断空闲线程池是否已满
+            if(freePool.size()<poolConfig.getMaxCon()){
+               //空闲线程没有满
+               //回收连接资源
+                freePool.add(connection);
+            }else {
+               //空闲线程已满
+                try {
+                    connection.close();
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+            }
+            activePool.remove(connection);
+            count--;
+            notifyAll();
+        }
     }
 }
